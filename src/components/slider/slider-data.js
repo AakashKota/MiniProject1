fa63a4ab29c9da38d6dda2752dec7cb023c46616
MiniProject2023 <kota.aakash15@gmail.com>
2023-05-02 18:31:21 +0530

    first commit

---
+++ src/components/slider/slider-data.js
@@ -0,0 +1,25 @@
+export const sliderData = [
+    {
+      image:"https://i.ibb.co/jwv2pNz/nz-mobile-hero-1-d.jpg",
+      heading: "",
+      desc: "Up to 30% off on all onsale proucts.",
+    },
+   
+    {
+      image: "https://i.ibb.co/YtXS3D1/R.png",
+      heading: "",
+      desc: "Up to 30% off on all onsale proucts.",
+    },
+    
+    {
+      image: "https://i.ibb.co/8032jXQ/Banner-adapt-2.jpg",
+      heading: "All Fashion",
+      desc: "Up to 30% off on all onsale proucts.",
+    },
+    
+    {
+      image: "https://i.ibb.co/W3CptGg/R.jpg",
+      heading: "",
+      desc: "Up to 30% off on all onsale proucts.",
+    },
+  ];
